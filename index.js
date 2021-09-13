c4fcb17a40b10071b568c99bb031128a18404615
Wayne Wee <wwayne.wee@gmail.com>
2021-09-13 09:39:28 +0800

    added elastic ip

--- index.js
+++ index.js
@@ -1,111 +1,118 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const { session } = require('./models/session');
-const { generateRandomName } = require('./modules/name-generator');
+const { session } = require("./models/session");
+const { generateRandomName } = require("./modules/name-generator");
 
 const app = express();
 const port = 8000;
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === "production";
 
-let origin = `http://localhost:3000`
+let origin = `http://localhost:3000`;
 
 if (production) {
-  origin = `http://ec2-18-219-147-206.us-east-2.compute.amazonaws.com`
+  origin = `http://18.189.74.209/`;
 }
 
 let corsOptions = {
   origin,
-  credentials: true
-}
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.get('/new/:boardSize', async (req, res, next) => {
+app.get("/new/:boardSize", async (req, res, next) => {
   try {
-    const playerName = generateRandomName()
-    const _session = await session._new(req.params.boardSize, playerName)
-    res.json({ session: _session, playerName })
-  } catch (e) {next(e)}
+    const playerName = generateRandomName();
+    const _session = await session._new(req.params.boardSize, playerName);
+    res.json({ session: _session, playerName });
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.get('/:sessionId', async (req, res, next) => {
- 
+app.get("/:sessionId", async (req, res, next) => {
   try {
-    let { playerName } = req.query
+    let { playerName } = req.query;
 
-    let _session = await session.findOne({ id: req.params.sessionId })
+    let _session = await session.findOne({ id: req.params.sessionId });
 
     if (!_session) {
-      return res.sendStatus(404)
+      return res.sendStatus(404);
     }
 
     if (
-      _session.homePlayerName 
-      && _session.awayPlayerName 
-      && (playerName != _session.homePlayerName && playerName != _session.awayPlayerName)) {
-        return res.sendStatus(403)
-      }
+      _session.homePlayerName &&
+      _session.awayPlayerName &&
+      playerName != _session.homePlayerName &&
+      playerName != _session.awayPlayerName
+    ) {
+      return res.sendStatus(403);
+    }
 
     if (!playerName) {
-      playerName = generateRandomName()
-      await session.updateOne({
-        id: req.params.sessionId
-      }, {
+      playerName = generateRandomName();
+      await session.updateOne(
+        {
+          id: req.params.sessionId,
+        },
+        {
+          $set: {
+            awayPlayerName: playerName,
+          },
+        }
+      );
+    }
+    res.json({ session: _session, playerName });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/:sessionId", async (req, res, next) => {
+  try {
+    const { i, j, piece } = req.query;
+
+    const _session = await session.findOne({ id: req.params.sessionId });
+    const { board } = _session;
+
+    board[i][j] = piece;
+
+    await session.updateOne(
+      {
+        id: req.params.sessionId,
+      },
+      {
         $set: {
-          awayPlayerName: playerName
-        }
-      })
-    }
-    res.json({ session: _session, playerName })
-  } catch (e) {next(e)}
-
-})
-
-app.post('/:sessionId', async (req, res, next) => {
-  try {
-    const { 
-      i, 
-      j,
-      piece
-    } = req.query
-    
-    const _session = await session.findOne({ id: req.params.sessionId })
-    const { board } = _session
-
-    board[i][j] = piece
-
-    await session.updateOne({
-      id: req.params.sessionId
-    }, {
-      $set: {
-        board,
-        latestMove: {
-          i,
-          j
-        }
+          board,
+          latestMove: {
+            i,
+            j,
+          },
+        },
       }
-    })
+    );
 
-    res.sendStatus(200)
-  } catch(e) { next(e) }
-})
+    res.sendStatus(200);
+  } catch (e) {
+    next(e);
+  }
+});
 
-app.delete('/:sessionId', async (req, res, next) => {
-
+app.delete("/:sessionId", async (req, res, next) => {
   try {
-    const _session = session.findOne({ _id: req.params.sessionId })
+    const _session = session.findOne({ _id: req.params.sessionId });
     if (_session) {
-      await session.deleteOne({ id: req.params.sessionId })
+      await session.deleteOne({ id: req.params.sessionId });
     }
-    res.sendStatus(200)
-
-  } catch(e) {next(e)}
-
-})
+    res.sendStatus(200);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.listen(port);
 
-mongoose.connect(`mongodb://127.0.0.1/tic-tac-toe`)
+mongoose.connect(`mongodb://127.0.0.1/tic-tac-toe`);
